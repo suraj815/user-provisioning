ee352ef0799776d573f3f08f2c6ecc731531df3b
suraj815 <surajy145@gmail.com>
2022-05-02 20:38:02 +0530

    Update validator.groovy

--- validator.groovy
+++ validator.groovy
@@ -4,8 +4,10 @@ def checkParamInputs(){
     // Check if all required params are valid
     def missingParams = [];
     if (!params.ENVIRONMENT) missingParams << "ENVIRONMENT"
-    if (!params.User_Id) missingParams << "User_Id"
-    if (!params.User_Email) missingParams << "User_Email"
+    if (!params['One Healthcare Id']) missingParams << "One Healthcare Id"
+    if (!params['Email Id']) missingParams << "Email Id"
+    if (!params['First Name']) missingParams << "First Name"
+    if (!params['Last Name']) missingParams << "Last Name"
     assert missingParams.isEmpty() : reportError("The following Parameters are required; check your Inputs: " + missingParams)
 
 }
